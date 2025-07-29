fd4621958b9561a2e0e42b73b051d23052b2c534
Mark Hobbs <miciramark3@gnail.com>
2025-07-29 20:48:04 +0300

    Clean up unused files and finalize Unauthorized route

--- src/components/unauthorised.jsx
+++
@@ -1,8 +0,0 @@
-export default function Unauthorized() {
-  return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h2> Access Denied</h2>
-      <p>You don’t have permission to view this page.</p>
-    </div>
-  );
-}
