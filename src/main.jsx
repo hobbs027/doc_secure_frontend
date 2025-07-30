ac4508189aa29cef6de622e911eedd423ff7d52d
Mark Hobbs <miciramark3@gnail.com>
2025-07-30 16:36:49 +0300

    🐛 Fix runtime crash and restore rendering flow

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <AuthProvider>
+     <AuthProvider>
         <App />
-    </AuthProvider>
+     </AuthProvider>
     </Router>
   </React.StrictMode>
 );
